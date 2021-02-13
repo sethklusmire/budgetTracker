9851b17e77e38f5dd80bcb8f29df6cddbc0cda6a
Seth Klusmire <sethklusmire@gmail.com>
2021-02-13 15:39:22 -0700

    readMe

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # budgetTracker
+
+## Description
+Budget Tracker that helps track data over a period of time. The main goal of this application is storing data in the cache files.
+
+## Table of Contents
+
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [License](#license)
+* [Question](#questions)
+
+## Installation
+Install necessary dependencies with npm i command.
+
+## Usage
+Application requiring npm packages express, mongoose, morgan and compression.
+
+## Screenshot
+![screenshot](public/assests/image/preview.png)
+
+## Contributing
+Seth Klusmire
+
+## License
+MIT
+
+## Links
+https://github.com/sethklusmire/fitnessTracker
+
+## Questions
+Email and Github profiles: [sethklusmire@gmail.com](sethklusmire@gmail.com)
+
+https://github.com/sethklusmire 
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,6 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+// add the files to cache
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
@@ -11,29 +12,26 @@ const FILES_TO_CACHE = [
   "/icons/icon-512x512.png",
 ];
 
-// install
 self.addEventListener("install", function (evt) {
-  // pre cache image data
   evt.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.addAll("/api/transaction"))
+    caches
+      .open(DATA_CACHE_NAME)
+      .then((cache) => cache.addAll("/api/transaction"))
   );
-    
-  // pre cache all static assets
+
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
 
-  // tell the browser to activate this service worker immediately once it
-  // has finished installing
   self.skipWaiting();
 });
 
 // activate
-self.addEventListener("activate", function(evt) {
+self.addEventListener("activate", function (evt) {
   evt.waitUntil(
-    caches.keys().then(keyList => {
+    caches.keys().then((keyList) => {
       return Promise.all(
-        keyList.map(key => {
+        keyList.map((key) => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
             console.log("Removing old cache data", key);
             return caches.delete(key);
@@ -46,33 +44,33 @@ self.addEventListener("activate", function(evt) {
   self.clients.claim();
 });
 
-// fetch
-self.addEventListener("fetch", function(evt) {
+self.addEventListener("fetch", function (evt) {
   if (evt.request.url.includes("/api/transaction")) {
     evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(evt.request)
-          .then(response => {
-            // If the response was good, clone it and store it in the cache.
-            if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
-            }
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
 
-            return response;
-          })
-          .catch(err => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(evt.request);
-          });
-      }).catch(err => console.log(err))
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
     );
 
     return;
   }
 
   evt.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.match(evt.request).then(response => {
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request).then((response) => {
         return response || fetch(evt.request);
       });
     })
